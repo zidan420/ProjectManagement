5536cfd6a852a8b615d576bcb51b59ab825ff94e
root <root@localhost.localdomain>
2022-12-29 03:06:10 +0600

    Animation added

--- headers/splash.h
+++ headers/splash.h
@@ -1,4 +1,50 @@
 
+void animate_text(char *str, int index, int count)
+{
+    char temp[100];
+    strcpy(temp, str);
+
+    if (count == strlen(str))
+        return;
+
+    if ('a' <= temp[index] && temp[index] <= 'z')
+    {
+        temp[index] -= ('a' - 'A');
+    }
+    else if ('A' <= temp[index] && temp[index] <= 'Z')
+    {
+        temp[index] += ('a' - 'A');
+    }
+
+    if (temp[strlen(str)-1] == '\\')
+    {
+        temp[strlen(str)-1] = '|';
+        str[strlen(str)-1] = '|';
+    }
+    else if (temp[strlen(str)-1] == '|')
+    {
+        temp[strlen(str)-1] = '/';
+        str[strlen(str)-1] = '/';
+    }
+    else if (temp[strlen(str)-1] == '/')
+    {
+        temp[strlen(str)-1] = '-';
+        str[strlen(str)-1] = '-';
+    }
+    else if (temp[strlen(str)-1] == '-')
+    {
+        temp[strlen(str)-1] = '\\';
+        str[strlen(str)-1] = '\\';
+    }
+
+    usleep(150*1000);
+
+    printf("%s\r", temp);
+    fflush(stdout);
+
+    animate_text(str, index+1, count+1);
+}
+
 void title(void)
 {
     cls();
@@ -21,9 +67,10 @@ void title(void)
     printf("$$ | \\_/ $$ |$$ |  $$ |$$ | \\$$ |$$ |  $$ |\\$$$$$$  |$$$$$$$$\\ $$ | \\_/ $$ |$$$$$$$$\\ $$ | \\$$ |   $$ |\n");
     printf("\\__|     \\__|\\__|  \\__|\\__|  \\__|\\__|  \\__| \\______/ \\________|\\__|     \\__|\\________|\\__|  \\__|   \\__|\n\n");
 
-    printf("\t\t\t\t%s~~%sPowered by Md Zidan Khan%s\n\n", KYEL, KMAG, KNRM);
+    printf("\t\t\t\t%s~~ %sPowered by Md Zidan Khan%s\n\n", KYEL, KMAG, KNRM);
 
-    csleep(3);
+    char str[] = "[*] Starting the Project Manager...\\";
+    animate_text(str, 0, 0);
 
     cls();
 }
